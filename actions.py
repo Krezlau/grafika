754133f332e0288a5dbdf781f059aa2623d4db81
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-04-06 20:38:18 +0200

    Initial commit

---
+++ actions.py
@@ -0,0 +1,43 @@
+﻿import numpy as np
+from math import *
+
+
+
+def translate(x, y, z, points):
+    translation_matrix = np.matrix([
+        [1, 0, 0, x],
+        [0, 1, 0, y],
+        [0, 0, 1, z],
+        [0, 0, 0, 1]
+    ])
+    translated_points = []
+    for point in points:
+        translated_points.append(np.dot(translation_matrix, point))
+    return translated_points
+
+def rotate(angle, axis, points):
+    if axis == 'x':
+        rotation_matrix = np.matrix([
+            [1, 0, 0, 0],
+            [0, cos(angle), -sin(angle), 0],
+            [0, sin(angle), cos(angle), 0],
+            [0, 0, 0, 1]
+        ])
+    if axis == 'y':
+        rotation_matrix = np.matrix([
+            [cos(angle), 0, sin(angle), 0],
+            [0, 1, 0, 0],
+            [-sin(angle), 0, cos(angle), 0],
+            [0, 0, 0, 1]
+        ])
+    if axis == 'z':
+        rotation_matrix = np.matrix([
+            [cos(angle), -sin(angle), 0, 0],
+            [sin(angle), cos(angle), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+    rotated_points = []
+    for point in points:
+        rotated_points.append(np.dot(rotation_matrix, point))
+    return rotated_points
---
+++ main.py
@@ -0,0 +1,138 @@
+﻿import pygame
+import numpy as np
+from math import *
+
+from actions import translate, rotate
+
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+
+WIDTH, HEIGHT = 1280, 720
+scale = 50
+angle = 0.01
+
+pygame.display.set_caption("3D projection in pygame!")
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+n = 0
+f = 1
+
+transformation_matrix = np.matrix([
+    [1/((WIDTH/HEIGHT) * tan(45)), 0, 0, 0],
+    [0, 1/tan(45), 0, 0],
+    [0, 0, f/(f-n), f*n/(n-f)],
+    [0, 0, 1, 0]
+])
+
+
+def project(points_to_project):
+    projected_points = []
+    for point in points_to_project:
+        projected_points.append(np.dot(transformation_matrix, point))
+    return projected_points
+
+points = []
+connections = []
+
+# all the cube vertices
+points.append(np.matrix([1, 1, 1, 1]).reshape((4, 1)))
+points.append(np.matrix([2, 1, 1, 1]).reshape((4, 1)))
+points.append(np.matrix([2, 1, 2, 1]).reshape((4, 1)))
+points.append(np.matrix([1, 1, 2, 1]).reshape((4, 1)))
+points.append(np.matrix([1, 2, 1, 1]).reshape((4, 1)))
+points.append(np.matrix([2, 2, 1, 1]).reshape((4, 1)))
+points.append(np.matrix([2, 2, 2, 1]).reshape((4, 1)))
+points.append(np.matrix([1, 2, 2, 1]).reshape((4, 1)))
+
+connections.append([1, 3, 4])
+connections.append([0, 2, 5])
+connections.append([1, 3, 6])
+connections.append([0, 2, 7])
+connections.append([0, 5, 7])
+connections.append([1, 4, 6])
+connections.append([2, 5, 7])
+connections.append([3, 4, 6])
+
+print(points)
+
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+clock = pygame.time.Clock()
+while True:
+
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+            # if event.key == pygame.K_d:
+            #     points = translate(1, 0, 0, points)
+            # if event.key == pygame.K_a:
+            #     points = translate(-1, 0, 0, points)
+            # if event.key == pygame.K_w:
+            #     points = translate(0, 0, 1, points)
+            # if event.key == pygame.K_s:
+            #     points = translate(0, 0, -1, points)
+            # if event.key == pygame.K_LSHIFT:
+            #     points = translate(0, 1, 0, points)
+            # if event.key == pygame.K_SPACE:
+            #     points = translate(0, -1, 0, points)
+            # if event.key == pygame.K_LEFT:
+            #     points = rotate(angle, 'y', points)
+            # if event.key == pygame.K_RIGHT:
+            #     points = rotate(-angle, 'y', points)
+            # if event.key == pygame.K_UP:
+            #     points = rotate(angle, 'x', points)
+            # if event.key == pygame.K_DOWN:
+            #     points = rotate(-angle, 'x', points)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        points = translate(0.1, 0, 0, points)
+    if keys[pygame.K_a]:
+        points = translate(-0.1, 0, 0, points)
+    if keys[pygame.K_w]:
+        # points = translate(0, 0, 1, points)
+        scale += 0.1
+    if keys[pygame.K_s]:
+        # points = translate(0, 0, -1, points)
+        scale -= 0.1
+    if keys[pygame.K_LSHIFT]:
+        points = translate(0, 0.1, 0, points)
+    if keys[pygame.K_SPACE]:
+        points = translate(0, -0.1, 0, points)
+    if keys[pygame.K_LEFT]:
+        points = rotate(angle, 'y', points)
+    if keys[pygame.K_RIGHT]:
+        points = rotate(-angle, 'y', points)
+    if keys[pygame.K_UP]:
+        points = rotate(angle, 'x', points)
+    if keys[pygame.K_DOWN]:
+        points = rotate(-angle, 'x', points)
+
+    screen.fill(BLACK)
+    
+    # project
+    projected_points = project(points)
+    print(projected_points)
+    
+    # draw
+    for i in range(len(points)):
+        x = points[i][0, 0] * scale + WIDTH/2
+        y = points[i][1, 0] * scale + HEIGHT/2
+        # pygame.draw.circle(screen, RED, (x, y), 5)
+        screen.blit(pygame.font.SysFont('Arial', 24).render(str(i), True, WHITE), (x, y))
+        for connection in connections[i]:
+            x2 = points[connection][0, 0] * scale + WIDTH/2
+            y2 = points[connection][1, 0] * scale + HEIGHT/2
+            pygame.draw.line(screen, WHITE, (x, y), (x2, y2), 2)
+    
+    pygame.display.update()
+    
