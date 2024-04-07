c500f8032af2b70070e7aa13fb806e9392060f5a
Krzysztof <krzysztof.andrzej.jurkowski@gmail.com>
2024-04-07 19:14:21 +0200

    rotations working

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ BLACK = (0, 0, 0)
 
 WIDTH, HEIGHT = 1280, 720
 scale = 50
-angle = 0.01
+angle = 0.025
 
 pygame.display.set_caption("3D projection in pygame!")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -29,7 +29,12 @@ transformation_matrix = np.matrix([
 def project(points_to_project):
     projected_points = []
     for point in points_to_project:
-        projected_points.append(np.dot(transformation_matrix, point))
+        
+        if point[2, 0] < 0:
+            projected_points.append(np.matrix([-1, -1, -1, -1]).reshape((4, 1)))
+        else:
+            projected_points.append(np.dot(transformation_matrix, point))
+            
     return projected_points
 
 points = []
@@ -89,50 +94,32 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
                 exit()
-            # if event.key == pygame.K_d:
-            #     points = translate(1, 0, 0, points)
-            # if event.key == pygame.K_a:
-            #     points = translate(-1, 0, 0, points)
-            # if event.key == pygame.K_w:
-            #     points = translate(0, 0, 1, points)
-            # if event.key == pygame.K_s:
-            #     points = translate(0, 0, -1, points)
-            # if event.key == pygame.K_LSHIFT:
-            #     points = translate(0, 1, 0, points)
-            # if event.key == pygame.K_SPACE:
-            #     points = translate(0, -1, 0, points)
-            # if event.key == pygame.K_LEFT:
-            #     points = rotate(angle, 'y', points)
-            # if event.key == pygame.K_RIGHT:
-            #     points = rotate(-angle, 'y', points)
-            # if event.key == pygame.K_UP:
-            #     points = rotate(angle, 'x', points)
-            # if event.key == pygame.K_DOWN:
-            #     points = rotate(-angle, 'x', points)
-
+                
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
-        points = translate(0.1, 0, 0, points)
-    if keys[pygame.K_a]:
         points = translate(-0.1, 0, 0, points)
+    if keys[pygame.K_a]:
+        points = translate(0.1, 0, 0, points)
     if keys[pygame.K_w]:
-        points = translate(0, 0, 0.1, points)
-        # scale += 0.1
-    if keys[pygame.K_s]:
         points = translate(0, 0, -0.1, points)
-        # scale -= 0.1
+    if keys[pygame.K_s]:
+        points = translate(0, 0, 0.1, points)
     if keys[pygame.K_LSHIFT]:
-        points = translate(0, 0.1, 0, points)
-    if keys[pygame.K_SPACE]:
         points = translate(0, -0.1, 0, points)
+    if keys[pygame.K_SPACE]:
+        points = translate(0, 0.1, 0, points)
     if keys[pygame.K_LEFT]:
         points = rotate(angle, 'y', points)
     if keys[pygame.K_RIGHT]:
         points = rotate(-angle, 'y', points)
     if keys[pygame.K_UP]:
-        points = rotate(angle, 'x', points)
-    if keys[pygame.K_DOWN]:
         points = rotate(-angle, 'x', points)
+    if keys[pygame.K_DOWN]:
+        points = rotate(angle, 'x', points)
+    if keys[pygame.K_q]:
+        points = rotate(angle, 'z', points)
+    if keys[pygame.K_e]:
+        points = rotate(-angle, 'z', points)
 
     screen.fill(BLACK)
     
@@ -141,14 +128,13 @@ while True:
     # print(projected_points)
     
     # draw
-    for i in range(len(points)):
-        print(points[i])
-        x = (projected_points[i][0, 0])/projected_points[i][3, 0] * 1000 + WIDTH/2
-        y = (projected_points[i][1, 0])/projected_points[i][3, 0] * 1000 + HEIGHT/2
+    for i in range(len(projected_points)):
+        x = (projected_points[i][0, 0])/projected_points[i][3, 0] * WIDTH + WIDTH/2
+        y = (projected_points[i][1, 0])/projected_points[i][3, 0] * HEIGHT + HEIGHT/2
         screen.blit(pygame.font.SysFont('Arial', 24).render(str(i), True, WHITE), (x, y))
         for connection in connections[i]:
-            x2 = (projected_points[connection][0, 0])/projected_points[connection][3, 0] * 1000 + WIDTH/2
-            y2 = (projected_points[connection][1, 0])/projected_points[connection][3, 0] * 1000 + HEIGHT/2
+            x2 = (projected_points[connection][0, 0])/projected_points[connection][3, 0] * WIDTH + WIDTH/2
+            y2 = (projected_points[connection][1, 0])/projected_points[connection][3, 0] * HEIGHT + HEIGHT/2
             pygame.draw.line(screen, WHITE, (x, y), (x2, y2), 2)
     
     pygame.display.update()
