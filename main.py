10dc657e8a24acb15653b0af0d53e18d877850f8
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2024-04-12 19:09:30 +0200

    clipping lines but it doesnt work

--- main.py
+++ main.py
@@ -31,16 +31,80 @@ transformation_matrix = np.matrix([
 ])
 
 
+INSIDE = 0
+LEFT = 1
+RIGHT = 2
+BOTTOM = 4
+TOP = 8
+xmin = 0
+xmax = 1
+ymin = 0
+ymax = 1
+
+
+def determine_code(point):
+    code = INSIDE
+    if point[0] < xmin:
+        code |= LEFT
+    elif point[0] > xmax:
+        code |= RIGHT
+    if point[1] < ymin:
+        code |= BOTTOM
+    elif point[1] > ymax:
+        code |= TOP
+    return code
+
+def draw(points, codes):
+    for i in range(len(points)):
+        for connection in connections[i]:
+            # both points are outside the screen
+            if codes[i] & codes[connection] != 0:
+                continue
+            if not (codes[i] | codes[connection]):
+                pygame.draw.line(screen, WHITE, (points[i][0] * WIDTH + WIDTH/2, points[i][1] * HEIGHT + HEIGHT/2), (points[connection][0] * WIDTH + WIDTH/2, points[connection][1] * HEIGHT + HEIGHT/2))
+            else:
+                x0 = points[i][0]
+                y0 = points[i][1]
+                x1 = points[connection][0]
+                y1 = points[connection][1]
+                
+                if codes[i] > codes[connection]:
+                    code = codes[i]
+                else:
+                    code = codes[connection]
+                
+                if code & TOP:
+                    x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0)
+                    y = ymax
+                    pygame.draw.line(screen, WHITE, (x0 * WIDTH + WIDTH/2, y0 * HEIGHT + HEIGHT/2), (x * WIDTH + WIDTH/2, y * HEIGHT + HEIGHT/2))
+                elif code & BOTTOM:
+                    x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0)
+                    y = ymin
+                    pygame.draw.line(screen, WHITE, (x0 * WIDTH + WIDTH/2, y0 * HEIGHT + HEIGHT/2), (x * WIDTH + WIDTH/2, y * HEIGHT + HEIGHT/2))
+                elif code & RIGHT:
+                    y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0)
+                    x = xmax
+                    pygame.draw.line(screen, WHITE, (x0 * WIDTH + WIDTH/2, y0 * HEIGHT + HEIGHT/2), (x * WIDTH + WIDTH/2, y * HEIGHT + HEIGHT/2))
+                elif code & LEFT:
+                    y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0)
+                    x = xmin
+                    pygame.draw.line(screen, WHITE, (x0 * WIDTH + WIDTH/2, y0 * HEIGHT + HEIGHT/2), (x * WIDTH + WIDTH/2, y * HEIGHT + HEIGHT/2))
+                
+
 def project(points_to_project):
     projected_points = []
+    codes = []
     for point in points_to_project:
         
-        if point[2, 0] < 0:
-            projected_points.append(np.matrix([-1, -1, -1, -1]).reshape((4, 1)))
-        else:
-            projected_points.append(np.dot(transformation_matrix, point))
-            
-    return projected_points
+        # if point[2, 0] < 0:
+        #     projected_points.append(np.matrix([-1, -1, -1, -1]).reshape((4, 1)))
+        # else:
+        #     projected_points.append(np.dot(transformation_matrix, point))
+        
+        projection = np.dot(transformation_matrix, point)
+        projected_points.append([projection[0, 0] / projection[3, 0], projection[0, 0] / projection[3, 0]])
+        codes.append(determine_code(projected_points[-1]))
+    return (projected_points, codes)
 
 (points, connections) = parse('scene1.obj')
 print(points)
@@ -110,23 +174,22 @@ while True:
     screen.fill(BLACK)
     
     # project
-    projected_points = project(points)
+    (projected_points, codes) = project(points)
     # print(projected_points)
     # print(transformation_matrix)
     print(fov)
+    draw(projected_points, codes)
     
     # draw
-    for i in range(len(projected_points)):
-        if (projected_points[i][1, 0] < -HEIGHT):
-            continue
-        x = (projected_points[i][0, 0])/projected_points[i][3, 0] * WIDTH + WIDTH/2
-        y = (projected_points[i][1, 0])/projected_points[i][3, 0] * HEIGHT + HEIGHT/2
-        # screen.blit(pygame.font.SysFont('Arial', 24).render(str(i), True, WHITE), (x, y))
-        # pygame.draw.circle(screen, RED, (x, y), 1)
-        for connection in connections[i]:
-            x2 = (projected_points[connection][0, 0])/projected_points[connection][3, 0] * WIDTH + WIDTH/2
-            y2 = (projected_points[connection][1, 0])/projected_points[connection][3, 0] * HEIGHT + HEIGHT/2
-            pygame.draw.line(screen, WHITE, (x, y), (x2, y2), 2)
+    # for i in range(len(projected_points)):
+    #     x = (projected_points[i][0, 0])/projected_points[i][3, 0] * WIDTH + WIDTH/2
+    #     y = (projected_points[i][1, 0])/projected_points[i][3, 0] * HEIGHT + HEIGHT/2
+    #     # screen.blit(pygame.font.SysFont('Arial', 24).render(str(i), True, WHITE), (x, y))
+    #     # pygame.draw.circle(screen, RED, (x, y), 1)
+    #     for connection in connections[i]:
+    #         x2 = (projected_points[connection][0, 0])/projected_points[connection][3, 0] * WIDTH + WIDTH/2
+    #         y2 = (projected_points[connection][1, 0])/projected_points[connection][3, 0] * HEIGHT + HEIGHT/2
+    #         pygame.draw.line(screen, WHITE, (x, y), (x2, y2), 2)
 
 
     pygame.display.update()
